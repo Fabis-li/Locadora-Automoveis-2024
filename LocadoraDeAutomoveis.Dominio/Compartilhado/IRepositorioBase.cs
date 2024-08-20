a7b0def236eb2b0263da7607f7268dade310bef2
Fabis-li <fabiano_slima@hotmail.com>
2024-08-20 14:43:39 -0300

    criado interface repositorio base

---
+++ LocadoraDeAutomoveis.Dominio/Compartilhado/IRepositorioBase.cs
@@ -0,0 +1,11 @@
+﻿namespace LocadoraDeAutomoveis.Dominio.Compartilhado
+{
+    public interface IRepositorioBase<TEntidade> where TEntidade : EntidadeBase
+    {
+        void Inserir(TEntidade registro);
+        bool Editar(TEntidade registro);
+        bool Excluir(TEntidade registro);
+        TEntidade SElecionarPorId(int id);
+        List<TEntidade> SelecionarTodos();
+    }
+}
