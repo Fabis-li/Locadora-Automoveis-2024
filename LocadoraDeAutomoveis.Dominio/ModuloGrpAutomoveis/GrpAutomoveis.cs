21cd76dc8cb5363d72421d363103d66657a128e3
Fabis-li <fabiano_slima@hotmail.com>
2024-08-20 14:52:29 -0300

    criado entidade grupo automoveis

---
+++ LocadoraDeAutomoveis.Dominio/ModuloGrpAutomoveis/GrpAutomoveis.cs
@@ -0,0 +1,17 @@
+﻿using LocadoraDeAutomoveis.Dominio.Compartilhado;
+
+namespace LocadoraDeAutomoveis.Dominio.ModuloGrpAutomoveis
+{
+    public class GrpAutomoveis : EntidadeBase
+    {
+        public string Nome { get; set; }
+
+        //lista de valores dos planos
+
+        public GrpAutomoveis() { }
+        public GrpAutomoveis(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
