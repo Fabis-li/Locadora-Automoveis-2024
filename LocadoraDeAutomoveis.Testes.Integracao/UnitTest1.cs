d02cf77175d5469fa819bffd9c3ca46bcd8f2b50
Fabis-li <fabiano_slima@hotmail.com>
2024-08-20 14:32:42 -0300

    add projetos

---
+++ LocadoraDeAutomoveis.Testes.Integracao/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LocadoraDeAutomoveis.Testes.Integracao
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
