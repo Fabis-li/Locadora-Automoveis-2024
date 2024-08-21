8b1636682ed94a74965c18db5c68db77c7b45a63
Fabis-li <fabiano_slima@hotmail.com>
2024-08-21 09:29:08 -0300

    implementado view model grupo automoveis

---
+++ LocadoraDeAutomoveis.WebApp/Models/GrpAutomoveisViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocadoraDeAutomoveis.WebApp.Models
+{
+    public class InserirGrpAutomoveisViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+    }
+
+    public class EditarGrpAutomoveisViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+    }
+
+    public class listarGrpAutomoveisViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+
+    public class DetalhesGrpAutomoveisViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+
+}
