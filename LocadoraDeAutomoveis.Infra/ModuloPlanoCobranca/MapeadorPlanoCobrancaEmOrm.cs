8068e7f655d3198ecfd9df607aa7ea70b516078d
Fabis-li <fabiano_slima@hotmail.com>
2024-08-24 12:58:36 -0300

    implementado mapeador e repositorio

--- LocadoraDeAutomoveis.Infra/ModuloPlanoCobranca/MapeadorPlanoCobrancaEmOrm.cs
+++ LocadoraDeAutomoveis.Infra/ModuloPlanoCobranca/MapeadorPlanoCobrancaEmOrm.cs
@@ -8,42 +8,65 @@ namespace LocadoraDeAutomoveis.Infra.ModuloPlanoCobranca
     {
         public void Configure(EntityTypeBuilder<PlanoCobranca> pBuilder)
         {
+            // Mapeamento da tabela
             pBuilder.ToTable("TBPlanoCobranca");
 
             // Configura o campo discriminador
-            pBuilder.HasDiscriminator<string>("TipoPlanoCobranca")
-                .HasValue<PlanoDiario>("Diario")
-                .HasValue<PlanoControlado>("Controlado")
-                .HasValue<PlanoLivre>("Livre");
+            pBuilder.HasDiscriminator<TipoPlanoCobranca>("TipoPlanoCobranca")
+                .HasValue<PlanoDiario>(TipoPlanoCobranca.Diario)
+                .HasValue<PlanoControlado>(TipoPlanoCobranca.Controlado) 
+                .HasValue<PlanoLivre>(TipoPlanoCobranca.Livre);
 
-            //Configuração das propriedades comuns
-           pBuilder.Property(p => p.NomePlano)
-               .IsRequired()
-               .HasColumnType("varchar(100)");
+            // Configuração das propriedades comuns
+            pBuilder.Property(p => p.NomePlano)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
 
-           pBuilder.Property(p => p.GrupoAutomovelId)
-               .IsRequired()
-               .HasColumnType("int");
+            pBuilder.Property(p => p.GrupoAutomovelId)
+                .IsRequired()
+                .HasColumnType("int");
+
+            pBuilder.Property(p => p.TipoPlanoCobranca)
+                .IsRequired()
+                .HasColumnName("TipoPlanoCobranca")
+                .HasColumnType("int");
 
             pBuilder.HasOne(p => p.GrupoAutomoveis)
                 .WithMany(g => g.PlanosCobranca)
                 .HasForeignKey(p => p.GrupoAutomovelId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            // Configuração das propriedades específicas
-            pBuilder.HasKey(p => p.Id);
+            // Configurações específicas para cada tipo derivado
+            pBuilder.HasKey(e => e.Id);
 
-            //Configurações para PlanoControlado
-            pBuilder.Property<PlanoControlado>(p => p.KmDisponivel)
+            // Configurações para PlanoControlado
+            pBuilder.Property<decimal?>("KmDisponivel")
                 .HasColumnName("KmDisponivel")
-                .HasColumnType("decimal(18,2)");
+                .HasPrecision(18, 2);
 
+            pBuilder.Property<decimal?>("PrecoDiarioControlado")
+                .HasColumnName("PrecoDiariaControlado")
+                .HasPrecision(18, 2);
 
+            pBuilder.Property<decimal?>("PrecoProKmExcedido")
+                .HasColumnName("PrecoProKmExcedido")
+                .HasPrecision(18, 2);
 
+            // Configurações para PlanoDiario
+            pBuilder.Property<decimal?>("PrecoDiariaDiario")
+                .HasColumnName("PrecoDiariaDiario")
+                .HasPrecision(18, 2);
 
+            pBuilder.Property<decimal?>("PrecoPorKm")
+                .HasColumnName("PrecoPorKm")
+                .HasPrecision(18, 2);
 
-
-
+            // Configurações para PlanoLivre
+            pBuilder.Property<decimal?>("PrecoDiariaLivre")
+                .HasColumnName("PrecoDiariaLivre")
+                .HasPrecision(18, 2);
         }
+
     }
+    
 }
---
+++ LocadoraDeAutomoveis.Infra/ModuloPlanoCobranca/RepositorioPlanoCobrancaemOrm.cs
@@ -0,0 +1,52 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloPlanoCobranca;
+using LocadoraDeAutomoveis.Infra.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeAutomoveis.Infra.ModuloPlanoCobranca
+{
+    public class RepositorioPlanoCobrancaemOrm : RepositorioBaseEmOrm<PlanoCobranca>, IRepositorioPlanoCobranca
+    {
+        public RepositorioPlanoCobrancaemOrm(LocadoraDeAutomoveisDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override DbSet<PlanoCobranca> ObterRegistros()
+        {
+            return dbContext.PlanosCobranca;
+        }
+
+        public List<PlanoCobranca> Filtrar(Func<PlanoCobranca, bool> predicate)
+        {
+            return ObterRegistros()
+                .Where(predicate)
+                .ToList();
+        }
+
+        public override PlanoCobranca SelecionarPorId(int id)
+        {
+            return ObterRegistros()
+                .FirstOrDefault(p => p.Id == id)!;
+        }
+
+        public PlanoControlado ObterPlanoControladoPorId(int id)
+        {
+            return ObterRegistros()
+                .OfType<PlanoControlado>()
+                .FirstOrDefault(p => p.Id == id)!;
+        }
+
+        public PlanoDiario ObterPlanoDiarioPorId(int id)
+        {
+            return ObterRegistros()
+                .OfType<PlanoDiario>()
+                .FirstOrDefault(p => p.Id == id)!;
+        }
+
+        public PlanoLivre ObterPlanoLivrePorId(int id)
+        {
+            return ObterRegistros()
+                .OfType<PlanoLivre>()
+                .FirstOrDefault(p => p.Id == id)!;
+        }
+    }
+}
