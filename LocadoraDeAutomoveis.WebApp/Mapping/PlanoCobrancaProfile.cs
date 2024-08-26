63eb972d0ef591eb60418aeb32251df39fdb4654
Fabis-li <fabiano_slima@hotmail.com>
2024-08-25 21:36:47 -0300

    implementado profile

---
+++ LocadoraDeAutomoveis.WebApp/Mapping/PlanoCobrancaProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using LocadoraDeAutomoveis.Dominio.ModuloPlanoCobranca;
+using LocadoraDeAutomoveis.WebApp.Models;
+
+namespace LocadoraDeAutomoveis.WebApp.Mapping
+{
+    public class PlanoCobrancaProfile : Profile
+    {
+        public PlanoCobrancaProfile()
+        {
+            CreateMap<InserirPlanoCobrancaViewModel, PlanoCobranca>();
+            CreateMap<EditarPlanoCobrancaViewModel, PlanoCobranca>();
+
+            CreateMap<PlanoCobranca, ListarPlanoCobrancaViewModel>()
+                .ForMember(
+                    dest => dest.GrupoAutomovel, 
+                    opt => opt.MapFrom(src => src.GrupoAutomovel!.Nome)
+                );
+
+            CreateMap<PlanoCobranca, DetalhesPlanoCobrancaViewModel>()
+                .ForMember(
+                    dest => dest.GrupoAutomovel,
+                    opt => opt.MapFrom(src => src.GrupoAutomovel!.Nome)
+                );
+
+            CreateMap<PlanoCobranca, EditarPlanoCobrancaViewModel>();
+        }
+    }
+}
