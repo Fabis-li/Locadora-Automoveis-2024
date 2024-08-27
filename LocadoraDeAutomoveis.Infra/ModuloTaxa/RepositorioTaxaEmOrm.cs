a347ec958a18e5513bce2080080b52d4898b9f4c
Fabis-li <fabiano_slima@hotmail.com>
2024-08-26 21:39:53 -0300

    implementado repositorio

---
+++ LocadoraDeAutomoveis.Infra/ModuloTaxa/RepositorioTaxaEmOrm.cs
@@ -0,0 +1,23 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloTaxa;
+using LocadoraDeAutomoveis.Infra.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeAutomoveis.Infra.ModuloTaxa
+{
+    public class RepositorioTaxaEmOrm : RepositorioBaseEmOrm<Taxa>, IRepositorioTaxa
+    {
+        public RepositorioTaxaEmOrm(LocadoraDeAutomoveisDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override DbSet<Taxa> ObterRegistros()
+        {
+            return dbContext.Taxas;    
+        }
+
+        public List<Taxa> Filtrar(Func<Taxa, bool> predicate)
+        {
+            return ObterRegistros().Where(predicate).ToList();
+        }
+    }
+}
