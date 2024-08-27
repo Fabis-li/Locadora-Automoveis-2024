d245f34999ac28fc2d95f6738f1217cf6d15a253
Fabis-li <fabiano_slima@hotmail.com>
2024-08-27 16:15:25 -0300

    implementado mapeamento para modulo cliente e repositorio

---
+++ LocadoraDeAutomoveis.Infra/ModuloCliente/MapeadorClienteEmOrm.cs
@@ -0,0 +1,63 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloCliente;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeAutomoveis.Infra.ModuloCleinte
+{
+    public class MapeadorClienteEmOrm : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> cBuilder)
+        {
+            cBuilder.ToTable("TBCliente");
+
+            cBuilder.Property(c => c.Id)
+                .IsRequired()
+                .HasColumnType("int")
+                .ValueGeneratedOnAdd();
+
+            cBuilder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            cBuilder.Property(c => c.Rg)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            cBuilder.Property(c => c.Cnh)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            cBuilder.Property(c => c.NumeroDocumento)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            cBuilder.Property(c => c.Telefone)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            cBuilder.Property(c => c.Cidade)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            cBuilder.Property(c => c.Estado)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            cBuilder.Property(c => c.Bairro)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            cBuilder.Property(c => c.Rua)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            cBuilder.Property(c => c.Numero)
+                .IsRequired()
+                .HasColumnType("varchar(10)");
+
+            cBuilder.Property(c => c.TipoCliente)
+                .IsRequired()
+                .HasColumnType("int");
+        }
+    }
+}
---
+++ LocadoraDeAutomoveis.Infra/ModuloCliente/RepositorioClienteEmOrm.cs
@@ -0,0 +1,28 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloCliente;
+using LocadoraDeAutomoveis.Infra.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeAutomoveis.Infra.ModuloCliente
+{
+    public class RepositorioClienteEmOrm : RepositorioBaseEmOrm<Cliente>, IRepositorioCliente
+    {
+        public RepositorioClienteEmOrm(LocadoraDeAutomoveisDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override DbSet<Cliente> ObterRegistros()
+        {
+            return dbContext.Clientes;
+        }
+
+        public List<Cliente> Filtrar(Func<Cliente, bool> predicate)
+        {
+            return ObterRegistros().Where(predicate).ToList();
+        }
+    }
+}
