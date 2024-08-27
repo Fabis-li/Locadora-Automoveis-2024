0f41bf2b271110d148abbb29cfcc810e53ef2802
Fabis-li <fabiano_slima@hotmail.com>
2024-08-27 11:54:20 -0300

    criado profile

---
+++ LocadoraDeAutomoveis.WebApp/Mapping/TaxaProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using LocadoraDeAutomoveis.Dominio.ModuloTaxa;
+using LocadoraDeAutomoveis.WebApp.Models;
+
+namespace LocadoraDeAutomoveis.WebApp.Mapping
+{
+    public class TaxaProfile : Profile
+    {
+        public TaxaProfile()
+        {
+            CreateMap<InserirTaxaViewModel, Taxa>();
+            CreateMap<EditarTaxaViewModel, Taxa>();
+
+            CreateMap<Taxa, ListarTaxaViewModel>()
+                .ForMember(
+                    dest => dest.TipoCobranca,
+                    opt => opt.MapFrom(t => t.TipoCobranca.ToString())
+                );
+
+            CreateMap<Taxa, DetalhesTaxaViewModel>()
+                .ForMember(
+                    dest => dest.TipoCobranca,
+                    opt => opt.MapFrom(t => t.TipoCobranca.ToString())
+                );
+
+            CreateMap<Taxa, EditarTaxaViewModel>();
+        }
+    }
+}
