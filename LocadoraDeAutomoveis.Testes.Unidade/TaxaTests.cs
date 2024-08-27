02100f814331a6e30ac3f1e218413eef1a9d4f56
Fabis-li <fabiano_slima@hotmail.com>
2024-08-26 21:31:05 -0300

    implementado teste de unidade

---
+++ LocadoraDeAutomoveis.Testes.Unidade/TaxaTests.cs
@@ -0,0 +1,85 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloTaxa;
+
+namespace LocadoraDeAutomoveis.Testes.Unidade
+{
+    [TestClass]
+    [TestCategory("Unidade")]
+    public class TaxaTests
+    {
+        [TestMethod]
+        public void Deve_Criar_Taxa_Valida()
+        {
+            var taxa = new Taxa
+                (
+                    "Taxa 1",
+                    10m,
+                    TipoCobrancaEnum.Diaria
+                );
+
+            var erros = taxa.Validar();
+
+            Assert.AreEqual(0, erros.Count);
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Taxa_Com_Erro_Nome()
+        {
+            var taxa = new Taxa
+                (
+                    "",
+                    10m,
+                    TipoCobrancaEnum.Diaria
+                );
+
+            var erros = taxa.Validar();
+
+            List<string> errosEsperados =
+            [
+                "O Nome é obrigatório",
+            ];
+
+            Assert.AreEqual(errosEsperados.Count, erros.Count);
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Taxa_Com_Erro_Valor()
+        {
+            var taxa = new Taxa
+                (
+                    "Taxa 1",
+                    0m,
+                    TipoCobrancaEnum.Diaria
+                );
+
+            var erros = taxa.Validar();
+
+            List<string> errosEsperados =
+            [
+                "O valor precisa ser ao menos 1",
+            ];
+
+            Assert.AreEqual(errosEsperados.Count, erros.Count);
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Taxa_Com_Erro_Nome_Valor()
+        {
+            var taxa = new Taxa
+                (
+                    "",
+                    0m,
+                    TipoCobrancaEnum.Diaria
+                );
+
+            var erros = taxa.Validar();
+
+            List<string> errosEsperados =
+            [
+                "O Nome é obrigatório",
+                "O valor precisa ser ao menos 1",
+            ];
+
+            Assert.AreEqual(errosEsperados.Count, erros.Count);
+        }
+    }
+}
