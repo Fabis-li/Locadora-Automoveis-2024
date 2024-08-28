b935a403acb7168bbfbf672f48f54ebec83db7b2
Fabis-li <fabiano_slima@hotmail.com>
2024-08-28 16:02:19 -0300

    implementado profile

---
+++ LocadoraDeAutomoveis.WebApp/Mapping/ConfiguracaoProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using LocadoraDeAutomoveis.Dominio.ModuloConfiguracao;
+using LocadoraDeAutomoveis.WebApp.Models;
+
+namespace LocadoraDeAutomoveis.WebApp.Mapping
+{
+    public class ConfiguracaoProfile : Profile
+    {
+        public ConfiguracaoProfile()
+        {
+            CreateMap<InserirConfiguracaoViewModel, Configuracao>();
+            CreateMap<EditarConfiguracaoViewModel, Configuracao>();
+            
+            CreateMap<Configuracao, DetalhesAutomovelViewModel>();
+            CreateMap<Configuracao, EditarConfiguracaoViewModel>();
+        }
+
+    }
+}
