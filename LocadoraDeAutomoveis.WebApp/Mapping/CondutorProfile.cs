cf54207097d849f532d09a85ac19b379d9715e8d
Fabis-li <fabiano_slima@hotmail.com>
2024-08-28 10:27:05 -0300

    implementado profile

---
+++ LocadoraDeAutomoveis.WebApp/Mapping/CondutorProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using LocadoraDeAutomoveis.Dominio.ModuloCondutor;
+using LocadoraDeAutomoveis.WebApp.Models;
+
+namespace LocadoraDeAutomoveis.WebApp.Mapping
+{
+    public class CondutorProfile : Profile
+    {
+        public CondutorProfile()
+        {
+            CreateMap<InserirCondutorViewModel, Condutor>();
+            CreateMap<EditarCondutorViewModel, Condutor>();
+
+            CreateMap<Condutor, ListarCondutorViewModel>()
+                .ForMember(dest => dest.Cliente, opt => opt.MapFrom(src => src.Cliente.Nome))
+                .ForMember(dest => dest.Cliente, opt => opt.MapFrom(src => src.Cliente.NumeroDocumento))
+                .ForMember(dest => dest.Cliente, opt => opt.MapFrom(src => src.Cliente.Telefone))
+                .ForMember(dest => dest.ClienteId, opt => opt.MapFrom(src => src.Cliente.Id));
+
+            CreateMap<Condutor, DetalhesCondutorViewModel>()
+                .ForMember(dest => dest.Cliente, opt => opt.MapFrom(src => src.Cliente.Nome))
+                .ForMember(dest => dest.Cliente, opt => opt.MapFrom(src => src.Cliente.NumeroDocumento))
+                .ForMember(dest => dest.Cliente, opt => opt.MapFrom(src => src.Cliente.Telefone))
+                .ForMember(dest => dest.ClienteId, opt => opt.MapFrom(src => src.Cliente.Id));
+
+            CreateMap<Condutor, EditarCondutorViewModel>();
+        }
+
+
+    }
+}
