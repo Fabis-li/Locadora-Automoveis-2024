c407ff899164f737cf662e0185d2ecac2ce2c372
Fabis-li <fabiano_slima@hotmail.com>
2024-08-28 10:25:59 -0300

    implementado camada de servico

---
+++ LocadoraDeAutomovies.Aplicacao/Servicos/CondutorService.cs
@@ -0,0 +1,73 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Dominio.ModuloCondutor;
+
+namespace LocadoraDeAutomovies.Aplicacao.Servicos
+{
+    public class CondutorService
+    {
+        private readonly IRepositorioCondutor repositorioCondutor;
+
+        public CondutorService(IRepositorioCondutor repositorioCondutor)
+        {
+            this.repositorioCondutor = repositorioCondutor;
+        }
+
+        public Result<Condutor> Inserir(Condutor condutor)
+        {
+            repositorioCondutor.Inserir(condutor);
+
+            return Result.Ok(condutor);
+        }
+
+        public Result<Condutor> Editar(Condutor condutoratualizado)
+        {
+            var condutor = repositorioCondutor.SelecionarPorId(condutoratualizado.Id);
+
+            if (condutor is null)
+                return Result.Fail("O condutor não foi encontrado!");
+
+            condutor.Nome = condutoratualizado.Nome;
+            condutor.Email = condutoratualizado.Email;
+            condutor.Cpf = condutoratualizado.Cpf;
+            condutor.Cnh = condutoratualizado.Cnh;
+            condutor.ValidaCnh = condutoratualizado.ValidaCnh;
+            condutor.Telefone = condutoratualizado.Telefone;
+            condutor.ClienteId = condutoratualizado.ClienteId;
+            condutor.Cliente = condutoratualizado.Cliente;
+            condutor.ClienteCondutor = condutoratualizado.ClienteCondutor;
+
+            repositorioCondutor.Editar(condutor);
+
+            return Result.Ok(condutor);
+        }
+
+        public Result<Condutor> Excluir(int condutorid)
+        {
+            var condutor = repositorioCondutor.SelecionarPorId(condutorid);
+
+            if (condutor is null)
+                return Result.Fail("O condutor não foi encontrado!");
+
+            repositorioCondutor.Excluir(condutor);
+
+            return Result.Ok(condutor);
+        }
+
+        public Result<Condutor> SelecionarPorId(int condutorid)
+        {
+            var condutor = repositorioCondutor.SelecionarPorId(condutorid);
+
+            if (condutor is null)
+                return Result.Fail("O condutor não foi encontrado!");
+
+            return Result.Ok(condutor);
+        }
+
+        public Result<List<Condutor>> SelecionarTodos()
+        {
+            var condutores = repositorioCondutor.SelecionarTodos();
+
+            return Result.Ok(condutores);
+        }
+    }
+}
