ed06c19a721ecdbebe49fd0d65926f129c58728d
Fabis-li <fabiano_slima@hotmail.com>
2024-08-28 10:25:16 -0300

    implementado repositorio e mapeador modulo condutor

---
+++ LocadoraDeAutomoveis.Infra/ModuloCondutor/MapeadorCondutorEmOrm.cs
@@ -0,0 +1,57 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloCondutor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeAutomoveis.Infra.ModuloCondutor
+{
+    public class MapeadorCondutorEmOrm : IEntityTypeConfiguration<Condutor>
+    {
+        public void Configure(EntityTypeBuilder<Condutor> cBuilder)
+        {
+            cBuilder.ToTable("TBCondutor");
+
+            cBuilder.Property(c => c.Id)
+                .HasColumnType("int")
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            cBuilder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            cBuilder.Property(c => c.Email)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            cBuilder.Property(c => c.Cpf)
+                .IsRequired()
+                .HasColumnType("varchar(11)");
+
+            cBuilder.Property(c => c.Cnh)
+                .IsRequired()
+                .HasColumnType("varchar(11)");
+
+            cBuilder.Property(c => c.ValidaCnh)
+                .IsRequired()
+                .HasColumnType("datetime");
+
+            cBuilder.Property(c => c.Telefone)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            cBuilder.Property(c => c.ClienteCondutor)
+                .IsRequired()
+                .HasColumnType("bit");
+
+            cBuilder.Property(c => c.ClienteId)
+                .IsRequired()
+                .HasColumnType("int");
+
+            cBuilder.HasOne(c => c.Cliente)
+                .WithMany(c => c.Condutores)
+                .HasForeignKey(c => c.ClienteId)
+                .HasForeignKey(c => c.ClienteId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ LocadoraDeAutomoveis.Infra/ModuloCondutor/RepositorioCondutorEmOrm.cs
@@ -0,0 +1,23 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloCondutor;
+using LocadoraDeAutomoveis.Infra.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeAutomoveis.Infra.ModuloCondutor
+{
+    public class RepositorioCondutorEmOrm : RepositorioBaseEmOrm<Condutor>, IRepositorioCondutor
+    {
+        public RepositorioCondutorEmOrm(LocadoraDeAutomoveisDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override DbSet<Condutor> ObterRegistros()
+        {
+            return dbContext.Condutores;
+        }
+
+        public List<Condutor> Filtrar(Func<Condutor, bool> predicate)
+        {
+            return ObterRegistros().Where(predicate).ToList();
+        }
+    }
+}
