abd8b5edb0f6b2cc95202b7daa33d8ee29aeb054
Fabis-li <fabiano_slima@hotmail.com>
2024-08-28 16:01:33 -0300

    implementado injeção de dependencia

--- LocadoraDeAutomoveis.WebApp/Program.cs
+++ LocadoraDeAutomoveis.WebApp/Program.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using LocadoraDeAutomoveis.Dominio.ModuloAutomoveis;
 using LocadoraDeAutomoveis.Dominio.ModuloCliente;
 using LocadoraDeAutomoveis.Dominio.ModuloCondutor;
+using LocadoraDeAutomoveis.Dominio.ModuloConfiguracao;
 using LocadoraDeAutomoveis.Dominio.ModuloGrpAutomoveis;
 using LocadoraDeAutomoveis.Dominio.ModuloPlanoCobranca;
 using LocadoraDeAutomoveis.Dominio.ModuloTaxa;
@@ -10,6 +11,7 @@ using LocadoraDeAutomoveis.Infra.Compartilhado;
 using LocadoraDeAutomoveis.Infra.ModuloAutomovel;
 using LocadoraDeAutomoveis.Infra.ModuloCliente;
 using LocadoraDeAutomoveis.Infra.ModuloCondutor;
+using LocadoraDeAutomoveis.Infra.ModuloConfiguracao;
 using LocadoraDeAutomoveis.Infra.ModuloGrupoAutomoveis;
 using LocadoraDeAutomoveis.Infra.ModuloPlanoCobranca;
 using LocadoraDeAutomoveis.Infra.ModuloTaxa;
@@ -31,6 +33,7 @@ namespace LocadoraDeAutomoveis.WebApp
             builder.Services.AddScoped<IRepositorioTaxa, RepositorioTaxaEmOrm>();
             builder.Services.AddScoped<IRepositorioCliente, RepositorioClienteEmOrm>();
             builder.Services.AddScoped<IRepositorioCondutor, RepositorioCondutorEmOrm>();
+            builder.Services.AddScoped<IRepositorioConfiguracao, RepositorioConfiguracaoEmOrm>();
 
             builder.Services.AddScoped<GrupoAutomoveisService>();
             builder.Services.AddScoped<AutomovelService>();
@@ -38,6 +41,7 @@ namespace LocadoraDeAutomoveis.WebApp
             builder.Services.AddScoped<TaxaService>();
             builder.Services.AddScoped<ClienteService>();
             builder.Services.AddScoped<CondutorService>();
+            builder.Services.AddScoped<ConfiguracaoService>();
 
             builder.Services.AddScoped<GrupoAutomoveisResolver>();
 
