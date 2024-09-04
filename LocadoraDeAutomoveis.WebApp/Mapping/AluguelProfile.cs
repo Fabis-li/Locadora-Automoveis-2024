44ec4914e76eb9dd9e4f0dee027ca78fb738be05
Fabis-li <fabiano_slima@hotmail.com>
2024-09-04 13:56:13 -0300

    criado profile

---
+++ LocadoraDeAutomoveis.WebApp/Mapping/AluguelProfile.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using LocadoraDeAutomoveis.Dominio.ModuloAluguel;
+using LocadoraDeAutomoveis.WebApp.Mapping.Resolvers;
+using LocadoraDeAutomoveis.WebApp.Models;
+
+namespace LocadoraDeAutomoveis.WebApp.Mapping
+{
+    public class AluguelProfile: Profile
+    {
+        public AluguelProfile()
+        {
+            CreateMap<InserirAluguelViewModel, Aluguel>()
+                .ForMember(dest => dest.TaxasEscolhidas, opt => opt.MapFrom(t => t.TaxasEscolhidas));
+
+            CreateMap<EditarAluguelViewModel, Aluguel>();
+
+            CreateMap<Aluguel, ListarAluguelViewModel>()
+                .ForMember(dest => dest.Cliente, opt => opt.MapFrom(src => src.Condutor.Cliente.Nome))
+                .ForMember(dest => dest.Automovel, opt => opt.MapFrom(src => src.Automovel.Modelo))
+                .ForMember(dest => dest.TipoPlanoCobranca, opt => opt.MapFrom(src => src.TipoPlanoCobranca.ToString()))
+                .ForMember(dest => dest.ValorTotal, opt => opt.MapFrom(src => src.ValorEntrada));
+
+            CreateMap<Aluguel, EditarAluguelViewModel>();
+
+            CreateMap<Aluguel, ConcluirAluguelViewModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.TipoPlanoCobranca, opt => opt.MapFrom(src => src.TipoPlanoCobranca.ToString()))
+                .ForMember(dest => dest.ValorTotal, opt => opt.MapFrom(src => src.ValorEntrada))
+                .ForMember(dest => dest.DataSaida, opt => opt.MapFrom(src => src.DataSaida))
+                .ForMember(dest => dest.DataRetorno, opt => opt.MapFrom(src => src.DataRetorno))
+                .ForMember(dest => dest.KmRodado, opt => opt.MapFrom(src => src.KmRodado))
+                .ForMember(dest => dest.MarcadorCombustivel, opt => opt.MapFrom(src => src.MarcadorCombustivel.ToString()));
+               
+
+            CreateMap<ConcluirAluguelViewModel, Aluguel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.TipoPlanoCobranca, opt => opt.MapFrom(src => src.TipoPlanoCobranca))
+                .ForMember(dest => dest.ValorEntrada, opt => opt.MapFrom(src => src.ValorTotal))
+                .ForMember(dest => dest.DataSaida, opt => opt.MapFrom(src => src.DataSaida))
+                .ForMember(dest => dest.DataRetorno, opt => opt.MapFrom(src => src.DataRetorno))
+                .ForMember(dest => dest.KmRodado, opt => opt.MapFrom(src => src.KmRodado))
+                .ForMember(dest => dest.MarcadorCombustivel, opt => opt.MapFrom(src => src.MarcadorCombustivel));
+
+
+        }
+    }
+}
---
+++ LocadoraDeAutomoveis.WebApp/Mapping/Resolvers/TaxaEscolhidaValueResolver.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LocadoraDeAutomoveis.Dominio.ModuloAluguel;
+using LocadoraDeAutomoveis.Dominio.ModuloTaxa;
+using LocadoraDeAutomoveis.WebApp.Models;
+
+namespace LocadoraDeAutomoveis.WebApp.Mapping.Resolvers
+{
+    //public class TaxaEscolhidaValueResolver : IValueResolver<InserirAluguelViewModel, Aluguel, List<Taxa>>
+    //{
+    //    private readonly IRepositorioTaxa repositorioTaxa;
+    //    public List<Taxa> Resolve(InserirAluguelViewModel source, Aluguel destination, List<Taxa> destMember, ResolutionContext context)
+    //    {
+    //        var idsTaxasEscolhidas = source.TaxasEscolhidas.ToList();
+
+    //        return repositorioTaxa.SelecionarMuito(idsTaxasEscolhidas);
+    //    }
+    //}
+}
