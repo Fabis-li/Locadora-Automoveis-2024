17a75db9e08eaad69e086cf06d6f59bfed1a2af4
Fabis-li <fabiano_slima@hotmail.com>
2024-09-06 17:46:12 -0300

    corrigidos servicos aluguel e planocobranca

--- LocadoraDeAutomovies.Aplicacao/Servicos/AluguelService.cs
+++ LocadoraDeAutomovies.Aplicacao/Servicos/AluguelService.cs
@@ -1,6 +1,7 @@
 ﻿using FluentResults;
 using LocadoraDeAutomoveis.Dominio.ModuloAluguel;
 using LocadoraDeAutomoveis.Dominio.ModuloAutomoveis;
+using LocadoraDeAutomoveis.Dominio.ModuloPlanoCobranca;
 using LocadoraDeAutomoveis.Infra.ModuloAutomovel;
 
 namespace LocadoraDeAutomovies.Aplicacao.Servicos
@@ -9,10 +10,13 @@ namespace LocadoraDeAutomovies.Aplicacao.Servicos
     {
         private readonly IRepositorioAluguel repositorioAluguel;
         private readonly IRepositorioAutomovel repositorioAutomovel;
+        private readonly IRepositorioPlanoCobranca repositorioPlanoCobranca;
 
-        public AluguelService(IRepositorioAluguel repositorioAluguel)
+        public AluguelService(IRepositorioAluguel repositorioAluguel, IRepositorioAutomovel repositorioAutomovel, IRepositorioPlanoCobranca repositorioPlanoCobranca)
         {
             this.repositorioAluguel = repositorioAluguel;
+            this.repositorioAutomovel = repositorioAutomovel;
+            this.repositorioPlanoCobranca = repositorioPlanoCobranca;
         }
 
         public Result<Aluguel> Inserir(Aluguel aluguel)
@@ -82,7 +86,7 @@ namespace LocadoraDeAutomovies.Aplicacao.Servicos
 
         public Result<Aluguel> Concluir(Aluguel aluguelDevolucao)
         {
-            if(aluguelDevolucao.DataRetorno is not null)
+            if(aluguelDevolucao.Status == StatusAluguelEnum.Concluido)
                 return Result.Fail("Aluguel já foi concluído!");
 
             ConcluirDevolucao(aluguelDevolucao);
@@ -90,6 +94,36 @@ namespace LocadoraDeAutomovies.Aplicacao.Servicos
             repositorioAluguel.Editar(aluguelDevolucao);
 
             return Result.Ok(aluguelDevolucao);
+        }
+
+        public Result<Aluguel> Devolver(Aluguel aluguelAtualizado)
+        {
+            var aluguel = repositorioAluguel.SelecionarPorId(aluguelAtualizado.Id);
+
+            if (aluguel is null)
+                return Result.Fail("Aluguel não encontrado!");
+
+            if (aluguelAtualizado.Status == StatusAluguelEnum.Concluido)
+                return Result.Fail("Aluguel já foi devolvido!");
+
+
+            aluguel.DataRetorno = aluguelAtualizado.DataRetorno;
+            aluguel.KmRodado = aluguelAtualizado.KmRodado;
+            aluguel.MarcadorCombustivel = aluguelAtualizado.MarcadorCombustivel;
+            aluguel.Status = StatusAluguelEnum.Concluido;
+
+            int planoId = (int)aluguel.TipoPlanoCobranca;
+
+            var planoCobranca = repositorioPlanoCobranca.SelecionarPorId(planoId);
+
+            if(planoCobranca is null)
+                return Result.Fail("Plano de cobrança não encontrado!");
+
+            var valorTotal = aluguel.CalcularValorTotal(planoCobranca);
+
+            repositorioAluguel.Editar(aluguel);
+
+            return Result.Ok(aluguel);
         }
 
         private void ConcluirDevolucao(Aluguel aluguel)
@@ -101,6 +135,10 @@ namespace LocadoraDeAutomovies.Aplicacao.Servicos
             aluguel.Devolucao();
 
             repositorioAutomovel.Editar(aluguel.Automovel!);
+
+           
         }
+
+       
     }
 }
