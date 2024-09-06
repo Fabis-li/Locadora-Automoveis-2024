9582de7812a06eb036f9365b31ead14584cac015
Fabis-li <fabiano_slima@hotmail.com>
2024-09-06 17:44:45 -0300

    corrido profile

--- LocadoraDeAutomoveis.WebApp/Mapping/AluguelProfile.cs
+++ LocadoraDeAutomoveis.WebApp/Mapping/AluguelProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using LocadoraDeAutomoveis.Dominio.ModuloAluguel;
+using LocadoraDeAutomoveis.Dominio.ModuloTaxa;
 using LocadoraDeAutomoveis.WebApp.Mapping.Resolvers;
 using LocadoraDeAutomoveis.WebApp.Models;
 
@@ -10,7 +11,7 @@ namespace LocadoraDeAutomoveis.WebApp.Mapping
         public AluguelProfile()
         {
             CreateMap<InserirAluguelViewModel, Aluguel>()
-                .ForMember(dest => dest.TaxasEscolhidas, opt => opt.MapFrom(t => t.TaxasEscolhidas));
+                .ForMember(dest => dest.TaxasEscolhidas, opt => opt.MapFrom<TaxaEscolhidaValueResolver>());
                 
 
             CreateMap<EditarAluguelViewModel, Aluguel>();
@@ -41,7 +42,22 @@ namespace LocadoraDeAutomoveis.WebApp.Mapping
                 .ForMember(dest => dest.KmRodado, opt => opt.MapFrom(src => src.KmRodado))
                 .ForMember(dest => dest.MarcadorCombustivel, opt => opt.MapFrom(src => src.MarcadorCombustivel));
 
+            CreateMap<Aluguel, DevolucaoAluguelViewModel>()
+                .ForMember(dest => dest.TaxasEscolhidas, opt => opt.MapFrom(src => src.TaxasEscolhidas.Select(tx => tx.Id)))                
+                .ForMember(dest => dest.Taxas, opt => opt.MapFrom<TaxasValueResolver>())
+                .ForMember(dest => dest.Condutores, opt => opt.MapFrom<CondutoresValueResolver>())
+                .ForMember(dest => dest.Automoveis, opt => opt.MapFrom<AutomovelValueResolver>())
+                .ForMember(v => v.ValorTotal, opt => opt.MapFrom<ValorTotalValueResolver>());
 
+            CreateMap<DevolucaoAluguelViewModel, Aluguel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.TaxasEscolhidas, opt => opt.MapFrom<TaxaEscolhidaValueResolver>())
+                .ForMember(dest => dest.TipoPlanoCobranca, opt => opt.MapFrom(src => src.TipoPlanoCobranca))
+                .ForMember(dest => dest.ValorEntrada, opt => opt.MapFrom(src => src.ValorTotal))
+                .ForMember(dest => dest.DataSaida, opt => opt.MapFrom(src => src.DataSaida))
+                .ForMember(dest => dest.DataRetorno, opt => opt.MapFrom(src => src.DataRetorno))
+                .ForMember(dest => dest.KmRodado, opt => opt.MapFrom(src => src.KmRodado))
+                .ForMember(dest => dest.MarcadorCombustivel, opt => opt.MapFrom(src => src.MarcadorCombustivel));
         }
     }
 }
