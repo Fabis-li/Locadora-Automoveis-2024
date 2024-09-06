e1112aa00257177705dd1806373770b77034e00e
Fabis-li <fabiano_slima@hotmail.com>
2024-09-06 17:45:16 -0300

    corrigido models

--- LocadoraDeAutomoveis.WebApp/Models/AluguelViewModel.cs
+++ LocadoraDeAutomoveis.WebApp/Models/AluguelViewModel.cs
@@ -1,25 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using LocadoraDeAutomoveis.Dominio.ModuloAluguel;
-using LocadoraDeAutomoveis.Dominio.ModuloAutomoveis;
+﻿using LocadoraDeAutomoveis.Dominio.ModuloAluguel;
 using LocadoraDeAutomoveis.Dominio.ModuloCliente;
-using LocadoraDeAutomoveis.Dominio.ModuloCondutor;
 using LocadoraDeAutomoveis.Dominio.ModuloPlanoCobranca;
-using LocadoraDeAutomoveis.Dominio.ModuloTaxa;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace LocadoraDeAutomoveis.WebApp.Models
 {
-    public class InserirAluguelViewModel
+    public class FormularioViewModel
     {
-        [Required(ErrorMessage = "O campo Cliente é obrigatório")]
-        public int ClienteId { get; set; }
-
         [Required(ErrorMessage = "O campo Automóvel é obrigatório")]
         public int AutomovelId { get; set; }
-
-        //[Required(ErrorMessage = "O campo Plano de Cobrança é obrigatório")]
-        //public int PlanoCobrancaId { get; set; }
-
+        
         [Required(ErrorMessage = "O campo Condutor é obrigatório")]
         public int CondutorId { get; set; }
 
@@ -42,60 +33,35 @@ namespace LocadoraDeAutomoveis.WebApp.Models
 
         [Required(ErrorMessage = "O tipo de plano é obrigatório")]
         public TipoPlanoCobrancaEnum TipoPlanoCobranca { get; set; }
+        public StatusAluguelEnum Status { get; set; }   
 
         [Required(ErrorMessage = "O campo Km Rodado é obrigatório")]
         public int KmRodado { get; set; }
 
-        public IEnumerable<Cliente> Clientes { get; set; }
+        public IEnumerable<int> TaxasEscolhidas { get; set; }
+
         public IEnumerable<SelectListItem> Automoveis { get; set; }
-        public IEnumerable<PlanoCobranca> PlanosCobranca { get; set; }
         public IEnumerable<SelectListItem> Condutores { get; set; }
-        public IEnumerable<SelectListItem> TaxasEscolhidas { get; set; }
+        public IEnumerable<SelectListItem> Taxas { get; set; }
 
     }
 
-    public class EditarAluguelViewModel
+    public class InserirAluguelViewModel : FormularioViewModel
+    {
+
+    }
+
+    public class EditarAluguelViewModel : FormularioViewModel
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "O campo Cliente é obrigatório")]
-        public int ClienteId { get; set; }
+    }
 
-        [Required(ErrorMessage = "O campo Automóvel é obrigatório")]
-        public int AutomovelId { get; set; }
+    public class DevolucaoAluguelViewModel : FormularioViewModel
+    {
+        public int Id { get; set; }
 
-        [Required(ErrorMessage = "O campo Plano de Cobrança é obrigatório")]
-        public int PlanoCobrancaId { get; set; }
-
-        [Required(ErrorMessage = "O campo Condutor é obrigatório")]
-        public int CondutorId { get; set; }
-
-        [Required(ErrorMessage = "O campo Data de Início é obrigatório")]
-        [DataType(DataType.Date)]
-        public DateTime DataSaida { get; set; }
-
-        [Required(ErrorMessage = "O campo Data de Fim é obrigatório")]
-        [DataType(DataType.Date)]
-        public DateTime DataRetorno { get; set; }
-
-        [Required(ErrorMessage = "O campo Valor Total é obrigatório")]
         public decimal ValorTotal { get; set; }
-
-        [Required(ErrorMessage = "O marcador de combustível é obrigatório")]
-        public MarcadorCombustivelEnum MarcadorCombustivel { get; set; }
-
-        [Required(ErrorMessage = "O tipo de plano é obrigatório")]
-        public TipoPlanoCobrancaEnum TipoPlanoCobranca { get; set; }
-
-        [Required(ErrorMessage = "O campo Km Rodado é obrigatório")]
-        public int KmRodado { get; set; }
-
-        public IEnumerable<Cliente> Clientes { get; set; }
-        public IEnumerable<SelectListItem> Automoveis { get; set; }
-        public IEnumerable<PlanoCobranca> PlanosCobranca { get; set; }
-        public IEnumerable<SelectListItem> Condutores { get; set; }
-        public IEnumerable<SelectListItem> TaxasEscolhidas { get; set; }
-
     }
 
     public class ListarAluguelViewModel
@@ -108,6 +74,7 @@ namespace LocadoraDeAutomoveis.WebApp.Models
         public DateTime DataRetorno { get; set; }
         public decimal ValorEntrada { get; set; }
         public decimal ValorTotal { get; set; }
+        public string Status { get; set; }
         public string TipoPlanoCobranca { get; set; }
         public string MarcadorCombustivel { get; set; }
         public int KmRodado { get; set; }
