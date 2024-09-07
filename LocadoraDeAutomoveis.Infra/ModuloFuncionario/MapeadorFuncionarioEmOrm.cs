e7c431daa14fe85afe93bb3c2e526f9241e587e2
Fabis-li <fabiano_slima@hotmail.com>
2024-09-07 02:11:03 -0300

    criado mapeamento do banco e repositorio

---
+++ LocadoraDeAutomoveis.Infra/ModuloFuncionario/MapeadorFuncionarioEmOrm.cs
@@ -0,0 +1,48 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloFuncionario;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeAutomoveis.Infra.ModuloFuncionario
+{
+    public class MapeadorFuncionarioEmOrm : IEntityTypeConfiguration<Funcionario>
+    {
+        public void Configure(EntityTypeBuilder<Funcionario> fBuilder)
+        {
+            fBuilder.ToTable("TBFuncionario");
+
+            fBuilder.Property(f => f.Id)
+                .HasColumnType("int")
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            fBuilder.Property(f => f.NomeCompleto)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            fBuilder.Property(f => f.Email)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            fBuilder.Property(f => f.Admissao)
+                .HasColumnType("datetime2")
+                .IsRequired();
+
+            fBuilder.Property(f => f.Salario)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            fBuilder.Property(f => f.EmpresaId)
+                .HasColumnType("int")
+                .IsRequired();
+
+            fBuilder.HasOne(f => f.Empresa)
+                .WithMany()
+                .HasForeignKey(e => e.EmpresaId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            fBuilder.Property(f => f.UsuarioId)
+                .HasColumnType("int")
+                .IsRequired();
+        }
+    }
+}
---
+++ LocadoraDeAutomoveis.Infra/ModuloFuncionario/RepositorioFuncionarioEmOrm.cs
@@ -0,0 +1,34 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloFuncionario;
+using LocadoraDeAutomoveis.Infra.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeAutomoveis.Infra.ModuloFuncionario
+{
+    public class RepositorioFuncionarioEmOrm : RepositorioBaseEmOrm<Funcionario>, IRepositorioFuncionario
+    {
+        public RepositorioFuncionarioEmOrm(LocadoraDeAutomoveisDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override DbSet<Funcionario> ObterRegistros()
+        {
+            return dbContext.Funcionarios;
+        }
+
+
+        public override Funcionario? SelecionarPorId(int id)
+        {
+            return dbContext.Funcionarios
+                .Include(f => f.Empresa)
+                .FirstOrDefault(f => f.Id == id);
+        }
+
+        public List<Funcionario> SelecionarTodos(Func<Funcionario, bool> predicate)
+        {
+            return dbContext.Funcionarios
+                .Include(f => f.Empresa)
+                .Where(predicate)
+                .ToList();
+        }
+    }
+}
