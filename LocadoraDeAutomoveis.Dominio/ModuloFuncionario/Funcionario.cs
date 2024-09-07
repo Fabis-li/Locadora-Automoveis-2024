52f74eac38d0fc685b74a36ad36b070f6caea995
Fabis-li <fabiano_slima@hotmail.com>
2024-09-07 02:09:23 -0300

    criado entidade e interface repositorio

---
+++ LocadoraDeAutomoveis.Dominio/ModuloFuncionario/Funcionario.cs
@@ -0,0 +1,40 @@
+﻿using LocadoraDeAutomoveis.Dominio.Compartilhado;
+using LocadoraDeAutomoveis.Dominio.ModuloAutenticacao;
+
+namespace LocadoraDeAutomoveis.Dominio.ModuloFuncionario
+{
+    public class Funcionario : EntidadeBase
+    {
+
+        public int UsuarioId { get; set; }
+
+        public int EmpresaId { get; set; }
+        public Usuario? Empresa { get; set; }
+
+
+        public string NomeCompleto { get; set; }
+        public string Email { get; set; }
+        public DateTime Admissao { get; set; }
+        public decimal Salario { get; set; }
+
+        protected Funcionario()
+        {
+            
+        }
+
+        public Funcionario( string nomeCompleto, string email, DateTime admissao, decimal salario, int empresaId)
+        {
+            NomeCompleto = nomeCompleto;
+            Email = email;
+            Admissao = admissao;
+            Salario = salario;
+            EmpresaId = empresaId;
+        }
+
+        public List<string> Validar()
+        {
+            return [];
+        }
+    }
+}
+    
---
+++ LocadoraDeAutomoveis.Dominio/ModuloFuncionario/IRepositorioFuncionario.cs
@@ -0,0 +1,11 @@
+﻿namespace LocadoraDeAutomoveis.Dominio.ModuloFuncionario
+{
+    public interface IRepositorioFuncionario
+    {
+        void Inserir(Funcionario funcionario);
+        void Editar(Funcionario funcionario);
+        void Excluir(Funcionario funcionario);
+        Funcionario? SelecionarPorId(int idSelecionado);
+        List<Funcionario> SelecionarTodos(Func<Funcionario, bool> predicate);
+    }
+}
