e6824982f75c1cba3211f040ac9a472a005b53cd
Fabis-li <fabiano_slima@hotmail.com>
2024-09-09 11:26:38 -0300

    criado resolver para empresa id

---
+++ LocadoraDeAutomoveis.WebApp/Mapping/Resolvers/EmpresaIdValueResolver.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Authentication;
+using AutoMapper;
+using LocadoraDeAutomovies.Aplicacao.Servicos;
+
+namespace LocadoraDeAutomoveis.WebApp.Mapping.Resolvers;
+
+public class EmpresaIdValueResolver : IValueResolver<object, object, int>
+{
+    private readonly AutenticacaoService autenticacaoService;
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public EmpresaIdValueResolver(AutenticacaoService autenticacaoService, IHttpContextAccessor httpContextAccessor)
+    {
+        this.autenticacaoService = autenticacaoService;
+        this.httpContextAccessor = httpContextAccessor;
+    }
+
+    public int Resolve(object source, object destination, int destMember, ResolutionContext context)
+    {
+        var usuarioClaim = httpContextAccessor.HttpContext?.User;
+
+        var empresaId = autenticacaoService.ObterIdEmpresaAsync(usuarioClaim).Result;
+
+        if (empresaId is null)
+            throw new AuthenticationException("Não foi possível obter o id da empresa!");
+
+
+        return empresaId.Value;
+    }
+}
