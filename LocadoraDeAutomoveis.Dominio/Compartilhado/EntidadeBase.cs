ed8025734dedda172da917932b8d54f2271c8ffc
Fabis-li <fabiano_slima@hotmail.com>
2024-09-09 11:02:21 -0300

    movimentado empresaid para entidade base

--- LocadoraDeAutomoveis.Dominio/Compartilhado/EntidadeBase.cs
+++ LocadoraDeAutomoveis.Dominio/Compartilhado/EntidadeBase.cs
@@ -1,7 +1,12 @@
-﻿namespace LocadoraDeAutomoveis.Dominio.Compartilhado
+﻿using LocadoraDeAutomoveis.Dominio.ModuloAutenticacao;
+
+namespace LocadoraDeAutomoveis.Dominio.Compartilhado
 {
     public abstract class EntidadeBase
     {
         public int Id { get; set; }
+
+        public int EmpresaId { get; set; }
+        public Usuario? Empresa { get; set; }
     }
 }
